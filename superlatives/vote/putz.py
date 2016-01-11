7ce3bb6fa3fff0a2faf45d174c5559f7d244a5cf
jmend <jmend@mit.edu>
2016-01-11 12:02:30 -0500

    Added find_putzen()

--- superlatives/vote/putz.py
+++ superlatives/vote/putz.py
@@ -6,6 +6,7 @@ with open("vote/putzen.json") as f:
 
 
 class Putzen(object):
+
     def __init__(self, input):
         self.name = input
 
@@ -23,6 +24,7 @@ class Putzen(object):
 
     @name.setter
     def name(self, name):
+        name = name.lower()
         if not isinstance(name, (str, unicode)):
             t = type(name)
             raise TypeError("Input name must be string, not " + str(t))
@@ -37,3 +39,22 @@ class Putzen(object):
     @name.deleter
     def name(self):
         raise Exception("Can't delete putzen!")
+
+
+def find_putzen(st):
+    ret = list()
+    for s in st.split():
+        try:
+            t = Putzen(s)
+            ret.append(t)
+        except KeyError:
+            pass
+    return ret
+
+
+if __name__ == "__main__":
+    assert Putzen("jmend").name == "jmend"
+    assert Putzen("lily").name == "lilyzhou"
+    assert Putzen("LILY").name == "lilyzhou"
+    assert Putzen("LILYzhou").name == "lilyzhou"
+    assert len(find_putzen("julian WEI WEI?")) == 3
